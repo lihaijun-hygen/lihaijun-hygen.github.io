23980b715bed9dae7faa6faf8f9c160e3886bf50
lihaijun-hygen <lihaijune@163.com>
2026-06-04 09:30:56 +0800

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'academic', 'awards']
+const section_names = ['home', 'publications', 'events', 'projects', 'awards']
 
 
 window.addEventListener('DOMContentLoaded', event => {
